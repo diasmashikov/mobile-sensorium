f7d26276a7e2ceb928beb78c01c32f7ad3bceeb5
diasmashikov <diasmashikovnasa@gmail.com>
2023-12-04 16:16:50 -0500

    Bloc deleted

--- lib/sensor_page.dart
+++ lib/sensor_page.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mobile_sensorium/accelerometer_data_display_page.dart';
-import 'package:mobile_sensorium/bloc/sensor_bloc/sensor_bloc.dart';
-import 'package:mobile_sensorium/bloc/sensor_bloc/sensor_event.dart';
-import 'package:mobile_sensorium/bloc/sensor_bloc/sensor_state.dart';
 import 'package:mobile_sensorium/database/accelerometer_db.dart';
 import 'package:mobile_sensorium/database/database_service.dart';
 import 'package:mobile_sensorium/model/accelerometer_record.dart';
 import 'package:mobile_sensorium/model/acceletometer_data.dart';
 import 'package:mobile_sensorium/orientation_manager.dart';
 import 'package:sensors_plus/sensors_plus.dart';
-import 'package:sqflite/sqflite.dart';
 
 class SensorPage extends StatefulWidget {
   const SensorPage({super.key});
@@ -20,84 +14,61 @@ class SensorPage extends StatefulWidget {
 }
 
 class _SensorPageState extends State<SensorPage> {
-  late SensorBloc sensorBloc;
-  bool isCollectingData = false;
-  String selectedAction = "walking";
-  List<String> actions = ["walking", "running"];
+  OrientationState _orientationState = OrientationState.portrait;
+  double pi = 3.14;
+  AccelerometerData _accelerometerData =
+      AccelerometerData(x: 0.0, y: 0.0, z: 0.0);
+  final OrientationManager _orientationManager = OrientationManager();
+  final accelerometerDB = AccelerometerDB();
 
   @override
   void initState() {
     super.initState();
-    sensorBloc = SensorBloc();
-  }
 
-  void toggleDataCollection() {
-    if (isCollectingData) {
-      sensorBloc.add(StopDataCollectionEvent());
-    } else {
-      sensorBloc.add(StartDataCollectionEvent());
-    }
-    setState(() {
-      isCollectingData = !isCollectingData;
+    accelerometerEventStream(samplingPeriod: SensorInterval.normalInterval)
+        .listen((event) async {
+      setState(() {
+        _accelerometerData =
+            AccelerometerData(x: event.x, y: event.y, z: event.z);
+
+        _orientationState =
+            _orientationManager.determineOrientation(_accelerometerData);
+      });
+
+      final record = AccelerometerRecord(
+          timestamp: DateTime.now().toString(),
+          x: _accelerometerData.x,
+          y: _accelerometerData.y,
+          z: _accelerometerData.z,
+          orientation: _orientationState.toString(),
+          action: "walking");
+
+      await accelerometerDB.createAccelerometerRecord(record);
     });
   }
 
   @override
-  void dispose() {
-    sensorBloc.close(); // Don't forget to close the BLoC
-    super.dispose();
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildOrientation(),
+        Divider(),
+        _buildMetrics(_accelerometerData),
+        OutlinedButton(
+            onPressed: () async {
+              List<AccelerometerRecord> records =
+                  await accelerometerDB.fetchAllAccelerometerRecords();
+              print(records);
+            },
+            child: const Text("Show accelerometer data"))
+      ],
+    ));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => sensorBloc,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocBuilder<SensorBloc, SensorDataState>(
-              builder: (context, state) {
-                return Text(
-                    'Orientation: ${state.orientationState.toString().split('.').last}');
-              },
-            ),
-            const Divider(),
-            BlocBuilder<SensorBloc, SensorDataState>(
-              builder: (context, state) {
-                return _buildMetrics(state.accelerometerData);
-              },
-            ),
-            DropdownButton<String>(
-              value: selectedAction,
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedAction = newValue!;
-                });
-              },
-              items: actions.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            OutlinedButton(
-              onPressed: toggleDataCollection,
-              child: Text(isCollectingData
-                  ? "Stop Collecting Data"
-                  : "Start Collecting Data"),
-            ),
-            OutlinedButton(
-              onPressed: () async {
-                // Clear database logic here
-              },
-              child: const Text("Clear database"),
-            ),
-          ],
-        ),
-      ),
-    );
+  Widget _buildOrientation() {
+    return Text('Orientation: ${_orientationState.toString().split('.').last}');
   }
 
   Widget _buildMetrics(AccelerometerData data) {
